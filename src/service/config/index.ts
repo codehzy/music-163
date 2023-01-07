ef098427b915ae87e668e28dad40a0cfe11ad485
codehzy <1257664828@qq.com>
2023-01-07 11:08:33 +0800

    feat(完成请求测试): ✨ 完成请求测试

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,3 +1,16 @@
 // 1.区分开发环境和生产环境
-export const BASE_URL = 'http://codercba.com:9002'
+// export const BASE_URL = 'http://codercba.com:9002'
 export const TIME_OUT = 10000
+
+// 依赖
+// let BASE_URL = ''
+// if (process.env.NODE_ENV === 'development') {
+//   BASE_URL = 'http://codercba.com:9002'
+// } else {
+//   BASE_URL = 'http://codercba.com:9002'
+// }
+// export { BASE_URL }
+
+// console.log(process.env.REACT_APP_BASE_URL)
+
+export const BASE_URL = process.env.REACT_APP_BASE_URL
